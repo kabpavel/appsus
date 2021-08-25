8e2fc3ae9821555ffc0e4570ac522eab74993181
yotke <yotam.elia@gmail.com>
2021-08-25 17:33:46 +0300

    mail changes preview

--- js/apps/mail/cmps/email-preview.jsx
+++ js/apps/mail/cmps/email-preview.jsx
@@ -1,4 +1,6 @@
 const { Link } = ReactRouterDOM
+
+import { Clock } from '../../../cmps/clock.jsx';
 // import { LongTxt } from './LongTxt.jsx'
 
 export function EmailPreview({ email, onSelectEmail }) {
@@ -7,9 +9,13 @@ export function EmailPreview({ email, onSelectEmail }) {
             <article onClick={() => { onSelectEmail(email) }} className="email-preview">
                 <h4>name - {email.name}</h4>
                 <h4>Title - {email.title}</h4>
-                <h4>content - {email.content}</h4>
+                <div className="email-clock">
+                    <Clock />
+                </div>
+                {/* <h4>content - {email.content}</h4> */}
                 {/* <LongTxt text={email.description} /> */}
             </article>
+            <hr className="email-hr" />
         </Link>
     )
 }
---
+++ js/cmps/clock.jsx
@@ -0,0 +1,24 @@
+
+export class Clock extends React.Component {
+    state = {
+        time: new Date()
+    };
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({ time: new Date() })
+        }, 1000);
+        // const month = new Date().getMonth() + 1;
+        // const newSeason = this.getSeason(month);
+        // this.setState({ season: newSeason });
+    }
+    render() {
+        const { time } = this.state
+        const hour = time.getHours() > 10 ? time.getHours() : '0' + time.getHours();
+        const minutes = time.getMinutes() > 10 ? time.getMinutes() : '0' + time.getMinutes();
+
+        return <div clock-container>
+            {hour+':'+minutes}
+        </div>
+    }
+
+}
