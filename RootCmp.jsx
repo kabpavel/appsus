08f2e8ee67295bb6aab24ad32b4ea8f600838ecd
pavel <kab.pavel@gmail.com>
2021-08-25 13:36:18 +0300

    second

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -4,6 +4,7 @@ const { Route, Switch } = ReactRouterDOM
 //import { AppHeader } from './cmps/AppHeader.jsx';
 // import { UserMsg } from './cmps/UserMsg.jsx';
 // import { About } from './pages/About.jsx';
+import { NoteApp } from './js/apps/keep/pages/note-app.jsx';
 import { AppsusApp } from './js/pages/appsus-app.jsx';
 import { Home } from './js/pages/app-home.jsx';
 export function App() {
@@ -14,6 +15,7 @@ export function App() {
       </header>
       <main>
         <Switch>
+          <Route path="/appsus/note-app" component={NoteApp} />
           <Route path="/appsus" component={AppsusApp} />
           <Route path="/" component={Home} />
         </Switch>
