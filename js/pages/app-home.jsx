f91b52f194f2474a53ade3a95ae99e659dbd6ee5
yotke <yotam.elia@gmail.com>
2021-08-28 22:16:23 +0300

    almost done

---
+++ js/pages/app-home.jsx
@@ -0,0 +1,10 @@
+import Welcome from 'react-welcome-page'
+
+
+export class Home extends React.Component {
+    render() {
+        return <div className="home-section">
+            <div className="welcome-container">Welcome To our app</div>
+        </div>
+    }
+}
