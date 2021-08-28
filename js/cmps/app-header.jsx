325549c02405e2bd9203db918c7710fe7e0cfbbe
yotke <yotam.elia@gmail.com>
2021-08-28 23:10:55 +0300

    on set images changes

--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -28,7 +28,7 @@ class _AppHeader extends React.Component {
                         <NavLink activeClassName="my-active" className={isOpen ? "open home" : "close home"} exact to="/" ><img src="assets/home.png" alt="" /></NavLink>
                         <NavLink activeClassName="my-active" className={isOpen ? "open email" : "close email"} to="/email" ><img src="assets/emailIcon.png" alt="" /></NavLink>
                         <NavLink activeClassName="my-active" className={isOpen ? "open keep" : "close keep"} to="/note-app" ><img src="assets/keep.ico" alt="" /></NavLink>
-                        <NavLink activeClassName="my-active" className={isOpen ? "open book" : "close book"} to="/miss-book/" >📚</NavLink>
+                        <NavLink activeClassName="my-active" className={isOpen ? "open book" : "close book"} to="/miss-book/" ><img src="assets/books.jpg" alt="" /></NavLink>
                         <NavLink activeClassName="my-active" className={isOpen ? "open about" : "close about"} to="/about" >About</NavLink>
                     </div>
                 </nav>
